ba8a7321285bb6b6552a5e8aae2506ac55c3bca2
maximBoolBool <remezmax117@gmail.com>
2024-01-15 20:28:10 +0300

    аыва

---
+++ RuleEntities/Entities/SpecialRules/RangeWeaponSpecialRule.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RuleEntities.Entities.SpecialRules;
+
+/// <summary>
+///     Спецправила для оружия дальнего боя
+/// </summary>
+[Table("range_weapon_special_rules")]
+public class RangeWeaponSpecialRule : BaseRuleEntity
+{
+    /// <summary>
+    ///     Id оружия
+    /// </summary>
+    [Column("weapon_id")]
+    [ForeignKey(nameof(Weapon))]
+    public long WeaponId { get; set; }
+    
+    /// <summary>
+    ///     Навигационное свойство
+    /// </summary>
+    public RangeWeaponEntity Weapon { get; set; }
+}
